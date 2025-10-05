6a93962d1bb20a7d9aa8aefdb47711567e34407a
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-05 13:39:19 +0200

    feat: DOM manejar los metodos importados

--- to-do/src/main.js
+++ to-do/src/main.js
@@ -1,24 +1,27 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import {addTask, getTasks} from './modules/todoManager.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const taskForm = document.getElementById('taskForm');
+const taskInput = document.getElementById('taskInput');
 
-setupCounter(document.querySelector('#counter'))
+
+taskForm.addEventListener('submit', (e) =>{
+  e.preventDefault(); //evitamos que la pagina se recarga
+  const texto = taskInput.value;
+  addTask(texto);
+  renderTask();
+  taskInput.value = '';
+  
+});
+
+const  taskList = document.getElementById('taskList');
+const renderTask = () =>{
+  let tasks = getTasks();
+  taskList.innerHTML = '';
+  tasks.forEach(task => {
+    let li = document.createElement('li');
+    li.textContent = task.description;
+    taskList.appendChild(li);
+  });
+}
+
+
